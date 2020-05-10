995e6eff1dbf037febe1df17846c39211e774e53
Aidan Melen <aidanmelen@Aidans-MacBook-Pro.local>
2020-05-10 17:20:48 -0600

    Bump version: 0.8.4 → 0.9.0

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/aidanmelen/awscli_bastion',
-    version='0.8.4',
+    version='0.9.0',
     zip_safe=False,
 )
