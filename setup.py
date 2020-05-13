7160e53af0aa6ee5513149f4080c8ef1a00bdcc8
Aidan Melen <aidanmelen@Aidans-MacBook-Pro.local>
2020-05-12 19:10:23 -0600

    readded missing click and humanize dependencies.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ 'boto3' ]
+requirements = [ 'Click', 'boto3', 'humanize' ]
 
 setup_requirements = [ 'sphinx', 'twine', 'bumpversion' ]
 
